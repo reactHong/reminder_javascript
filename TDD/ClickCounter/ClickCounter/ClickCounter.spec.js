df37eefe7cd5eb27fdaba9e38377d2c52bc23dab
Hong Shik Kim <hong@hongs.dev>
2021-03-23 20:51:58 +0900

    feat: Add increase function with testing codes

--- TDD/ClickCounter/ClickCounter/ClickCounter.spec.js
+++ TDD/ClickCounter/ClickCounter/ClickCounter.spec.js
@@ -1,8 +1,22 @@
 describe('App.ClickCounter', ()=> {
-  describe('getValue()', ()=> {
+  describe('getValue()', () => {
     it('초기값이 0인 카운터 값을 반환한다', ()=> {
       const counter = App.ClickCounter()
       expect(counter.getValue()).toBe(0)
+    })
+  })
+
+  describe('increase()', () => {
+    it('카운터를 1 올린다', () => {
+      // 준비
+      const counter = App.ClickCounter()
+
+      // 실행
+      counter.increase()
+
+      // 단언
+      expect(counter.getValue()).toBe(1)
+
     })
   })
 })
