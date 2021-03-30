97dbcbd4c902aefbdf4d1a7add721b2876201d90
Hong Shik Kim <hong@hongs.dev>
2021-03-30 23:28:54 +0900

    [TDD][ClickCounter] feat: Add testing updateView function

--- TDD/ClickCounter/ClickCounter/ClickCountView.spec.js
+++ TDD/ClickCounter/ClickCounter/ClickCountView.spec.js
@@ -1,7 +1,17 @@
 describe('App.ClickCountView 모듈의', () => {
+  let updateEl, clickCounter, view
+  beforeEach(() => {
+    clickCounter = App.ClickCounter()
+    updateEl = document.createElement('span')
+    view = App.ClickCountView(clickCounter, updateEl)
+
+    console.log(view);
+  })
   describe('updateView()는', () => {
     it('ClickCounter의 getValue() 값을 출력한다', () => {
-      //TODO
+      const counterValue = clickCounter.getValue()
+      view.updateView()
+      expect(updateEl.innerHTML).toBe(counterValue.toString())
     })
   })
 })
