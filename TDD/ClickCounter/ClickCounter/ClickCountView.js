d336c3a8348c213ffece871bf156fdffc5dbbb29
Hong Shik Kim <hong@hongs.dev>
2021-03-31 20:43:18 +0900

    [TDD][ClickCounter] feat: Add a test to check if clickCounter exists

--- TDD/ClickCounter/ClickCounter/ClickCountView.js
+++ TDD/ClickCounter/ClickCounter/ClickCountView.js
@@ -1,6 +1,8 @@
 var App = App || {}
 
 App.ClickCountView = (clickCounter, updateEl) => {
+  if (!clickCounter) throw Error("Error: ClickCounter is null!")
+
   return {
     updateView() {
       updateEl.innerHTML = clickCounter.getValue()
--- TDD/ClickCounter/ClickCounter/ClickCountView.spec.js
+++ TDD/ClickCounter/ClickCounter/ClickCountView.spec.js
@@ -4,9 +4,20 @@ describe('App.ClickCountView 모듈의', () => {
     clickCounter = App.ClickCounter()
     updateEl = document.createElement('span')
     view = App.ClickCountView(clickCounter, updateEl)
-
-    console.log(view);
   })
+
+  it('clickCounter를 주입하지 않으면 에러를 던진다', () => {
+    const clickCounter = null
+    const updateEl = document.createElement('span')
+
+    const actual = () => App.ClickCountView(clickCounter, updateEl)
+    expect(actual).toThrowError()
+  })
+
+  // it('updateEl를 주입하지 않으면 에러를 던진다', () => {
+
+  // })
+
   describe('updateView()는', () => {
     it('ClickCounter의 getValue() 값을 출력한다', () => {
       const counterValue = clickCounter.getValue()
