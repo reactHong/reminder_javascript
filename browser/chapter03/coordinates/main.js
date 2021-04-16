e2e43c6ae4fe8a5fd5674f903c422b371d95807b
Hong Shik Kim <hong@hongs.dev>
2021-04-17 08:34:11 +0900

    refactor: Improve performance by using transform CSS property instead of top, left property

--- browser/chapter03/coordinates/main.js
+++ browser/chapter03/coordinates/main.js
@@ -3,12 +3,22 @@ const vertLine = document.querySelector('.vertLine');
 const target = document.querySelector('.target');
 const coordinate = document.querySelector('.coordinate');
 
-document.addEventListener('mousemove', (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
-  horzLine.style.top = `${y}px`;
-  vertLine.style.left = `${x}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  coordinate.innerHTML = `(${x}, ${y})`;
+window.addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+
+  document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    console.log(targetHalfWidth);
+    console.log(targetHalfHeight);
+    horzLine.style.transform = `translateY(${y}px)`;
+    vertLine.style.transform = `translateX(${x}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    coordinate.innerHTML = `(${x}, ${y})`;
+  });
 });
