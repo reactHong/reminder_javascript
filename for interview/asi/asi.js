ca300ed7ad75c60c5228faae283b31fd4bc6b059
Hong Shik Kim <hong@hongs.dev>
2021-06-19 21:29:04 +0900

    [for interview] Add an example about ASI (Automatic Semicolon Insertion)

---
+++ for interview/asi/asi.js
@@ -0,0 +1,25 @@
+//Reference: https://youtu.be/MF-vhTCHHXA
+
+let a = {
+    a: 123,
+  }[a].forEach(function (x) {
+    console.log(x);
+  });
+
+// let b;
+// b = { a: 123 }[b].forEach(function (x) {
+//   console.log(x);
+// });
+
+// let c = {
+//   a: 123,
+// }['a'];
+// console.log(c);
+
+// {
+//   a: [1, 2, 3]
+// }['a'].forEach((x) => console.log(x));
+
+// ({
+//   a: [1, 2, 3]
+// }['a']).forEach((x) => console.log(x));
