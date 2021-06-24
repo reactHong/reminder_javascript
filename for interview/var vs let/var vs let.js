0a9dd73c62f889667903306dc99b3342fb9d3380
Hong Shik Kim <hong@hongs.dev>
2021-06-24 21:37:14 +0900

    [for interview] Add an example about var and let

---
+++ for interview/var vs let/var vs let.js
@@ -0,0 +1,22 @@
+//var hoisting vs let hoisting
+//WHAT will be the output from this code and why?
+
+function f() {
+  // area: undefined - let area = undefined
+  // name: not defined, not assign any default value - let name;
+
+  if (area === undefined)       console.log('var', area);
+  
+  // console.log(abcd);
+  console.log(typeof abcd);
+  if (typeof abcd === 'undefined') console.log('abcd is not defined')
+  
+  // Reference Error - because name is used in Temporal Dead Zone
+  if (typeof name == 'undefined') console.log('name is not defined');
+  console.log('let', name);
+  name = 'Bert';
+
+  let name = 'Bert';
+  var area = 'Geology';
+}
+f();
