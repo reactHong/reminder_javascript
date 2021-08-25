88f36107af60f14fb437b3266c3be7f858b7d3ce
Hong Shik Kim <mushoot@gmail.com>
2021-08-26 08:26:47 +0900

    [vanillajs-router] feat: Implement router with history.pushState

--- vanillajs-router/frontend/static/js/index.js
+++ vanillajs-router/frontend/static/js/index.js
@@ -1,18 +1,42 @@
 const router = async () => {
-
   const routes = [
-    { route: "/", view: () => console.log("Viewing Home") },
-    { route: "posts", view: () => console.log("Viewing Home") },
-    { route: "/", view: () => console.log("Viewing Home") },
+    { path: "/", view: () => console.log("Viewing Home") },
+    { path: "/posts", view: () => console.log("Viewing Posts") },
+    { path: "/settings", view: () => console.log("Viewing Setting") },
   ];
 
+  const potentialMatches = routes.map(route => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path
+    }
+  });
+
+  let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+  
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true,
+    }
+  }
+
+  match.route.view();
 };
 
-
-
-
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
 
 document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      navigateTo(e.target.href);
+      e.preventDefault();
+    }
+  });
+
   router();
 });
 
